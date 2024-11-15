3b98f7bdbc2bae8a71a14bb123716f7fc09aec8f
claudiuignat <77777213+claudiuignat@users.noreply.github.com>
2024-11-15 11:06:46 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -6,3 +6,16 @@ Name        = "My S3 Bucket"
 Environment = "Dev"
 }
 }
+
+# Define the encryption key for the volume
+resource "aws_kms_key" "my_aes_key" {
+  description = "KMS key for AES volume encryption"
+  key_usage   = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+}
+
+# EC2 Key Pair (Optional: to connect to the instance via SSH)
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "my-key-pair"
+  public_key = file("~/.ssh/id_rsa.pub") # Replace with your SSH public key path
+}
